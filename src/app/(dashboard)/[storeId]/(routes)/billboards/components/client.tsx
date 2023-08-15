6e2c08730968a0ffc7a7f2259dac006e0ec1a472
Ryan Skolnick <rhskolnick@gmail.com>
2023-08-15 15:15:45 -0400

    Finished API List component and updated middleware

--- src/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,6 +9,7 @@ import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
 import { BillboardColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
 interface BillboardClientProps {
 	data: BillboardColumn[];
 }
@@ -35,6 +36,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
 			</div>
 			<Separator />
 			<DataTable columns={columns} data={data} searchKey='label' />
+			<Heading title='API' description='API calls for billboards' />
+			<Separator />
+			<ApiList entityName='billboards' entityIdName='billboardId' />
 		</>
 	);
 };
